bb18ac1c60255048a94c3798f770a8600ea84e80
JUHI NAGPURE <juhinagpure143@gmail.com>
2023-03-27 21:38:09 +0530

    added array in perosnList.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,34 @@
 import './App.css';
 import { Greet } from './components/Greet'
 import { Person } from './components/Person'
+import { PersonList } from './components/PersonList'
 
 function App() {
   const personName = {
     first :'Bruce',
     last: 'Wayne'
   }
+
+  const nameList = [
+    {
+      first: 'Bruce',
+      last : 'Wayne',
+    },
+    {
+      first: 'Clark',
+      last: 'kent',
+    },
+    {
+      first: 'Princess',
+      last: 'Diana',
+    },
+]
+
   return (
     <div className="App">
       <Greet name = 'vishwas' messageCount={20} isLoggedIn={false}/>
       <Person name={personName} />
+      <PersonList names ={nameList}/>
     </div>
   )
 }
