1c2a2808071513a0179303f2599566b45d296f77
JUHI NAGPURE <juhinagpure143@gmail.com>
2023-03-28 10:49:34 +0530

    git commit -m added some advance props for passing components like status component

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,40 @@
 import './App.css';
-import { Greet } from './components/Greet'
-import { Person } from './components/Person'
-import { PersonList } from './components/PersonList'
+import {Status} from './components/Status'
+
 
 function App() {
-  const personName = {
-    first :'Bruce',
-    last: 'Wayne'
-  }
+//   const personName = {
+//     first :'Bruce',
+//     last: 'Wayne'
+//   }
 
-  const nameList = [
-    {
-      first: 'Bruce',
-      last : 'Wayne',
-    },
-    {
-      first: 'Clark',
-      last: 'kent',
-    },
-    {
-      first: 'Princess',
-      last: 'Diana',
-    },
-]
+//   const nameList = [
+//     {
+//       first: 'Bruce',
+//       last : 'Wayne',
+//     },
+//     {
+//       first: 'Clark',
+//       last: 'kent',
+//     },
+//     {
+//       first: 'Princess',
+//       last: 'Diana',
+//     },
+// ]
 
-  return (
-    <div className="App">
-      <Greet name = 'vishwas' messageCount={20} isLoggedIn={false}/>
-      <Person name={personName} />
-      <PersonList names ={nameList}/>
-    </div>
-  )
+//   return (
+//     <div className="App">
+//       <Greet name = 'vishwas' messageCount={20} isLoggedIn={false}/>
+//       <Person name={personName} />
+//       <PersonList names ={nameList}/>
+//     </div>
+//   )
+return(
+ <div className='App'>
+<Status status='loading'/>
+</div>
+)
 }
 
 export default App;
