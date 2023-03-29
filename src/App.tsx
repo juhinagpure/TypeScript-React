9bf30c76077857134ea3bc782cb3ea7d03d21ddb
JUHI NAGPURE <juhinagpure143@gmail.com>
2023-03-29 18:21:52 +0530

    tried some examples on event props

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import './App.css'
-import { Button } from './components/Button'
+
+import { Input } from './components/input'
 
 function App (){
-  return (
+  return(
     <div className='App'>
-<Button handleClick={(event ,id)=>{
-  console.log('Button clicked',event ,id)
-}}
-/>
+<Input value='' handleChange={(event)=> console.log(event)}/>
     </div>
   )
 }
+
+
+
+
+export default App
