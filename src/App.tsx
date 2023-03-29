a0cd92299dd14f6347f4956277e85d34ebb775a5
JUHI NAGPURE <juhinagpure143@gmail.com>
2023-03-29 17:45:22 +0530

    added new file input.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,13 @@
-import './App.css';
-// import{ Greet} from './components/Greet'
-// import { Heading } from './components/heading';
-// import { Oscar } from './components/Oscar';
-// import {Status} from './components/Status';
-import {Button} from './components/Button';
+import './App.css'
+import { Button } from './components/Button'
 
-
-function App() {
-return (
-<div className='App'>
-  <button handleClick={()=>{
- console.log('Button Clicked')
+function App (){
+  return (
+    <div className='App'>
+<Button handleClick={(event ,id)=>{
+  console.log('Button clicked',event ,id)
 }}
 />
-</div>
-)
+    </div>
+  )
 }
-
-export default App;
