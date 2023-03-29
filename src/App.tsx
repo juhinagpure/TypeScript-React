6c55b1ef9abc0850b47f5b0ac8627228031804bf
JUHI NAGPURE <juhinagpure143@gmail.com>
2023-03-29 17:38:51 +0530

    added new file button.tsx , here i learn event props

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,18 @@
 import './App.css';
-import{ Greet} from './components/Greet'
-import { Heading } from './components/heading';
-import { Oscar } from './components/Oscar';
-import {Status} from './components/Status'
+// import{ Greet} from './components/Greet'
+// import { Heading } from './components/heading';
+// import { Oscar } from './components/Oscar';
+// import {Status} from './components/Status';
+import {Button} from './components/Button';
 
 
 function App() {
-//   const personName = {
-//     first :'Bruce',
-//     last: 'Wayne'
-//   }
-
-//   const nameList = [
-//     {
-//       first: 'Bruce',
-//       last : 'Wayne',
-//     },
-//     {
-//       first: 'Clark',
-//       last: 'kent',
-//     },
-//     {
-//       first: 'Princess',
-//       last: 'Diana',
-//     },
-// ]
-
-//   return (
-//     <div className="App">
-//       <Greet name = 'vishwas' messageCount={20} isLoggedIn={false}/>
-//       <Person name={personName} />
-//       <PersonList names ={nameList}/>
-//     </div>
-//   )
-return(
- <div className='App'>
-<Status status='loading'/>
-<Heading>Placeholder text</Heading>
-<Oscar>
-  <Heading>Oscar goes to Leonardo Dicpario!</Heading>
-</Oscar>
-<Greet name = 'vishwas' isLoggedIn={false}/>
-
+return (
+<div className='App'>
+  <button handleClick={()=>{
+ console.log('Button Clicked')
+}}
+/>
 </div>
 )
 }
