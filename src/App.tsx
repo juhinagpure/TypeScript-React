0c7a7402fbc697cdd2ef60db58970af974b46728
JUHI NAGPURE <juhinagpure143@gmail.com>
2023-03-29 18:30:32 +0530

    import input component to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { Input } from './components/Input'
+import { Input   } from './components/Input'
 
 function App (){
   return(
@@ -8,8 +8,6 @@ function App (){
     </div>
   )
 }
-
-
 
 
 export default App
