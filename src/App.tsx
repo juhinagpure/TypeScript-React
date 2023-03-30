cd70f6df74f2799a7e6267ee531d90474bc311fa
JUHI NAGPURE <juhinagpure143@gmail.com>
2023-03-30 08:29:11 +0530

    added one new file container.tsx to learn styles props

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import './App.css'
-import { Input   } from './components/Input'
-
+import { Container } from './components/Container'
 function App (){
   return(
     <div className='App'>
-<Input value='' handleChange={(event)=> console.log(event)}/>
+<Container styles={{border:'1px solid black',padding:'1rem'}}/>
     </div>
   )
 }
