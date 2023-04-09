f409ef703620e2079b45591595e5610446db8b8b
JUHI NAGPURE <juhinagpure143@gmail.com>
2023-04-09 21:57:26 +0530

    use assertion without object

--- src/components/state/user.tsx
+++ src/components/state/user.tsx
@@ -19,8 +19,8 @@ export const User = () => {
         <div>
             <button onClick={handleLogin}>Login</button>
             {/* <button onClick={handleLogout}>Logout</button> */}
-            <div>User name is {user?.name}</div>
-            <div>User email is {user?.email}</div>
+            <div>User name is {user.name}</div>
+            <div>User email is {user.email}</div>
 
         </div>
     )
