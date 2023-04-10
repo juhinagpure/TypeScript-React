c534d5edc47e1a30f4bc8e608b78814b7c31afc0
JUHI NAGPURE <juhinagpure143@gmail.com>
2023-04-10 23:41:21 +0530

    used useReducer Strict Action Types

--- src/components/state/Counter.tsx
+++ src/components/state/Counter.tsx
@@ -1,14 +1,25 @@
 // import { type } from "os";
+import { type } from "os";
 import { useReducer } from "react";
 
 type CounterState = {
     count : number
 }
 
-type CounterAction = {
-    type:string
+type UpdateAction = {
+    type:'increment' | 'decrement'
     payload:number
 }
+
+type ResetAction = {
+    type:'reset'
+}
+type CounterAction = UpdateAction | ResetAction
+
+// type CounterAction = {
+//     type:'increment' | 'decrement' |'reset'
+//     payload:number
+// }
 const initialState = {count:0}
 
 function reducer(state:CounterState,action:CounterAction) {
@@ -17,6 +28,9 @@ function reducer(state:CounterState,action:CounterAction) {
             return {count: state.count + action.payload}
             case 'decrement':
                 return {count: state.count - action.payload}
+case 'reset':
+    return initialState
+
                 default:
                     return state
     }
@@ -30,6 +44,7 @@ export const counter = () => {
         <button onClick={() => dispatch({type:'increment', payload:10})}
         >Increment 10</button>
         <button onClick={() => dispatch({type:'decrement', payload:10})}>Decrement 10</button>
+        <button onClick={() => dispatch ({type:'reset'})}>Reset</button>
         </>
     )
 }
